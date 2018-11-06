c8996cddf9f97eaeca0b2bc57f52290657ef3834
rebyoliveira <rebecca.d.oliveira@gmail.com>
2018-11-06 18:10:33 +0200

    new test without error

--- src/CheckItem.test.js
+++ src/CheckItem.test.js
@@ -5,10 +5,12 @@ import { shallow } from 'enzyme';
 
 const products = [
   {
+    id: 1,
     name: '1 kg banana',
     done: true,
   },
   {
+    id: 2,
     name: '1 kg apple',
     done: false,
   },
@@ -17,11 +19,6 @@ const products = [
 it('[smoke] renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<CheckItem products={products} />, div);
-});
-
-it('[smoke] renders without products array and no crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CheckItem />, div);
 });
 
 it('[shallow] renders without crashing', () => {
